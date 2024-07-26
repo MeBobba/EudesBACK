2798f2326b171fb42df85265980fd9154b8dcd88
Ayrton BARDIOT <ayrton.bardiot@campus-centre.fr>
2024-07-26 22:48:37 +0200

    removed content in server.js cause of refactor

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,15 +3,6 @@ const router = express.Router();
 const {verifyToken} = require("../middlewares/authMiddleware");
 const postController = require("../controllers/postController");
 
-// router.get('/', verifyToken, articleController.getArticles);
-// router.get('/:id', verifyToken, articleController.getArticle);
-// router.post('/:articleId/likes', verifyToken, articleController.addLike);
-// router.post('/:articleId/comments', verifyToken, articleController.addComment);
-// router.post('/', verifyToken, articleController.createArticle);
-// router.delete('/:articleId/comments/:commentId', verifyToken, articleController.deleteComment);
-// router.delete('/:id', verifyToken, articleController.deleteArticle);
-// router.put('/:id', verifyToken, articleController.updateArticle);
-
 router.get('/:userId', verifyToken, postController.getPostsForUser);
 router.get('/public', verifyToken, postController.getPublicPosts);
 router.post('/', verifyToken, postController.createPost);
