ff11ac4bf5d38c90f858cd16e39c952179c5020c
Ayrton BARDIOT <ayrton.bardiot@campus-centre.fr>
2024-07-26 17:58:59 +0200

    remove env from git

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '84.54.32.4',
-    user: 'mebobbaprod',
-    password: 'RPsBXtPItVBa22VE',
-    database: 'mebobbaprod',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
