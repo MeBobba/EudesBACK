eb5dc20468267b39d2f7b6fc100a26fba03c7bde
Swayn <36993464+SwaynIO@users.noreply.github.com>
2024-07-29 21:06:10 +0200

    Adding LockScreen

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,22 +2,20 @@ const db = require("../db");
 const bcrypt = require("bcryptjs");
 const twofactor = require("node-2fa");
 const jwt = require("jsonwebtoken");
-const {getClientIp, checkBan} = require("../utils");
+const { getClientIp, checkBan } = require("../utils");
 
 const secretKey = process.env.SECRET_KEY || 'yourSecretKey';
 
 exports.login = async (req, res) => {
-    const {username, password, token2fa, machine_id} = req.body;
+    const { username, password, token2fa, machine_id } = req.body;
     const ip = getClientIp(req);
 
     try {
         // TODO: implémenter validation schema avec yup pour que ce soit plus simple
-        // Si pas de nom d'utilisateur, retourner une erreur
         if (!username) {
             return res.status(400).send('Username is required');
         }
 
-        // Si pas de mot de passe, retourner une erreur
         if (!password) {
             return res.status(400).send('Password is required');
         }
@@ -29,19 +27,16 @@ exports.login = async (req, res) => {
 
         const user = results[0];
 
-        // Vérifier les bannissements
         const isBanned = await checkBan(user.id, ip, machine_id);
         if (isBanned) {
             return res.status(403).send('User is banned');
         }
 
-        // Vérifier le mot de passe
         const passwordIsValid = await bcrypt.compare(password, user.password);
         if (!passwordIsValid) {
             return res.status(401).send('Invalid password');
         }
 
-        // Vérifier le token 2FA si activé
         if (user.is_2fa_enabled) {
             const verified = twofactor.verifyToken(user.google_auth_secret, token2fa);
             if (!verified) {
@@ -49,10 +44,10 @@ exports.login = async (req, res) => {
             }
         }
 
-        const token = jwt.sign({id: user.id, rank: user.rank}, secretKey, {expiresIn: '24h'});
+        const token = jwt.sign({ id: user.id, rank: user.rank }, secretKey, { expiresIn: '24h' });
 
         await db.query('UPDATE users SET machine_id = ? WHERE id = ?', [machine_id, user.id]);
-        res.status(200).send({auth: true, token});
+        res.status(200).send({ auth: true, token });
     } catch (err) {
         console.error('Error logging in user:', err);
         res.status(500).send('Server error');
@@ -70,7 +65,7 @@ exports.logout = async (req, res) => {
 };
 
 exports.register = async (req, res) => {
-    const {username, password, mail, machine_id} = req.body;
+    const { username, password, mail, machine_id } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 8);
     const account_created = Math.floor(Date.now() / 1000);
     const last_login = account_created;
@@ -82,8 +77,8 @@ exports.register = async (req, res) => {
             'INSERT INTO users (username, password, mail, account_created, last_login, motto, ip_register, ip_current, machine_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
             [username, hashedPassword, mail, account_created, last_login, motto, ip, ip, machine_id]
         );
-        const token = jwt.sign({id: result.insertId}, secretKey, {expiresIn: '24h'});
-        res.status(200).send({auth: true, token});
+        const token = jwt.sign({ id: result.insertId }, secretKey, { expiresIn: '24h' });
+        res.status(200).send({ auth: true, token });
     } catch (err) {
         console.error('Error registering user:', err);
         res.status(500).send('Server error');
@@ -91,7 +86,7 @@ exports.register = async (req, res) => {
 };
 
 exports.checkSession = (req, res) => {
-    res.status(200).send({valid: true});
+    res.status(200).send({ valid: true });
 };
 
 function generateAntiRobotQuestion() {
@@ -119,6 +114,26 @@ exports.checkBan = async (req, res) => {
         res.status(200).send('User is not banned');
     } catch (err) {
         console.error('Error checking ban status:', err);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.unlock = async (req, res) => {
+    const { password } = req.body;
+    const userId = req.userId; // assurez-vous que req.userId est bien défini par le middleware verifyToken
+    try {
+        const [results] = await db.query('SELECT password FROM users WHERE id = ?', [userId]);
+        if (results.length === 0) {
+            return res.status(404).send('User not found');
+        }
+        const user = results[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).send('Invalid password');
+        }
+        res.status(200).send({ success: true });
+    } catch (err) {
+        console.error('Error unlocking:', err);
         res.status(500).send('Server error');
     }
 };
